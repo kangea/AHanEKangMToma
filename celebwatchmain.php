38d2facfe9459458b026f20c59b1d56ca9520d72
kangea <kangea@bc.edu>
2016-04-08 17:01:02 -0400

    Main Page
    Added the signup form

--- celebwatchmain.php
+++ celebwatchmain.php
@@ -12,10 +12,17 @@
 		<div class="text-center">
 			<h1>Welcome to CelebWatch!</h1><br>
 			<div class="text-center">
-				<input type="button" class="btn btn-primary btn-lg" name="signup" value="Sign Up Here">
-				<input type="button" class="btn btn-primary btn-lg" name="login" value="Already a member? Login">
+				<form method="get">
+					<input type="submit" class="btn btn-primary btn-lg" name="signup" value="Sign Up Here">
+					<input type="submit" class="btn btn-primary btn-lg" name="login" value="Already a member? Login">
+				</form>
 			</div>
 		</div>
+<?php
+			if (isset($_GET['signup'])){
+				displaySignup();
+			}
+?>
 	</body>
 
 	<nav class="navbar navbar-default navbar-fixed-bottom">
@@ -27,7 +34,44 @@
 <?php
 	function displaySignup(){
 ?>
-
+		<form class="form-horizontal" action="dbconn.php">
+			<fieldset>
+				<legend>Sign-up Form</legend>
+				<div class="form-group">
+					<label for="inputName" class="col-lg-2 control-label">Name</label>
+					<div class="col-lg-10">
+						<input type="text" class="form-control" name="name" id="inputName" placeholder="Full Name">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="inputEmail" class="col-lg-2 control-label">Email</label>
+					<div class="col-lg-10">
+						<input type="text" class="form-control" name="email" id="inputEmail" placeholder="Email">
+					</div>
+				</div>
+				<div class="form-group">
+			      <label for="inputPassword" class="col-lg-2 control-label">Password</label>
+			      <div class="col-lg-10">
+			        <input type="password" class="form-control" name="password" id="inputPassword" placeholder="Password">
+			      </div>
+			      <label for="verifyPassword" class="col-lg-2 control-label">Verify Password</label>
+			      <div class="col-lg-10">
+			        <input type="password" class="form-control" id="verifyPassword" placeholder="Retype Password">
+			      </div>
+			    </div>
+			    <div class="checkbox">
+			    	<label>
+			    		<input type="checkbox"> I agree to the Terms and Conditions.
+			    	</label>
+			    </div>
+			    <div class="form-group">
+			    	<div class="col-lg-10 col-lg-offset-2">
+				        <button type="reset" class="btn btn-default">Cancel</button>
+				        <button type="submit" class="btn btn-primary">Submit</button>
+			    	</div>
+			    </div>
+			</fieldset>
+		</form>
 <?php
 	}
 ?>
