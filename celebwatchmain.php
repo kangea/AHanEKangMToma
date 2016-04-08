853440eb0a44d03709f1ab96d2ef885e7cea237a
kangea <kangea@bc.edu>
2016-04-08 16:10:44 -0400

    Main Page

---
+++ celebwatchmain.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title>CelebWatch</title>
+		<link rel="stylesheet" type="text/css" href="https://bootswatch.com/lumen/bootstrap.min.css">
+	</head>
+
+	<body>
+		<div class="jumbotron">
+			<h1>Welcome to CelebWatch!</h1><br>
+			<div class="btn-group-vertical">
+				<input type="button" class="btn btn-primary btn-lg" name="signup" value="Sign Up Here"><br>
+				<input type="button" class="btn btn-primary btn-lg" name="login" value="Already a member? Login">
+			</div>
+		</div>
+	</body>
+
+	<footer>
+		<p>&copy; 2016 Angela Han, Eunice Kang, Matthew Toma.</p>
+	</footer>
+
+</html>
+
+<?php
+	function displaySignup(){
+?>
+		
+<?php
+	}
+?>
