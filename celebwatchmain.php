a220011a3043ce7d9d6bcf6bd6f950e5406d1507
kangea <kangea@bc.edu>
2016-04-08 16:24:22 -0400

    ekang update

--- celebwatchmain.php
+++ celebwatchmain.php
@@ -4,29 +4,30 @@
 	<head>
 		<meta charset="utf-8">
 		<title>CelebWatch</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="https://bootswatch.com/lumen/bootstrap.min.css">
 	</head>
 
 	<body>
-		<div class="jumbotron">
+		<div class="container">
 			<h1>Welcome to CelebWatch!</h1><br>
 			<div class="btn-group-vertical">
-				<input type="button" class="btn btn-primary btn-lg" name="signup" value="Sign Up Here"><br>
+				<input type="button" class="btn btn-primary btn-lg" name="signup" value="Sign Up Here">
 				<input type="button" class="btn btn-primary btn-lg" name="login" value="Already a member? Login">
 			</div>
 		</div>
 	</body>
 
-	<footer>
+	<nav class="navbar navbar-default navbar-fixed-bottom">
 		<p>&copy; 2016 Angela Han, Eunice Kang, Matthew Toma.</p>
-	</footer>
+	</nav
 
 </html>
 
 <?php
 	function displaySignup(){
 ?>
-		
+
 <?php
 	}
 ?>
