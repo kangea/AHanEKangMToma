f10d2893549af01c14ce683a8890ed7b7d3f0016
an931a <hanav@bc.edu>
2016-04-14 15:42:32 -0400

    SQL

---
+++ project.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS Users (
+  Name VARCHAR(50) NOT NULL,
+  Password VARCHAR(20) NOT NULL,
+  Email VARCHAR(50) NOT NULL,
+  Passwordcheck VARCHAR(20) NOT NULL
+) 
+
+INSERT INTO Users (Name, Password, Email)
+
+CREATE TABLE IF NOT EXISTS Celebrities (
+	Name VARCHAR(100) NOT NULL,
+	Facebook VARCHAR(500) NOT NULL,
+	Twitter VARCHAR(500) NOT NULL,
+	Instagram VARCHAR(500) NOT NULL
+)
+	
+
+
