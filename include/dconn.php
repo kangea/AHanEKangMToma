e1db8ea5864a851aaefeacd27fbbf6257bf0658d
kangea <kangea@bc.edu>
2016-04-19 17:48:44 -0400

    Login Form Display
    also handle signup form

--- include/dconn.php
+++ include/dconn.php
@@ -20,35 +20,4 @@ function perform_query( $dbc, $query ){
 			die( "bad query".mysqli_error( $dbc ) );
 	return $result;
 }
-
-// Get the variables from the signup form
-	$dbc= connect_to_db( "hanav" );
-	$join_name =  $_POST['name'];
-	$join_email = $_POST['email'];
-	$join_password = $_POST['password'];
-	$join_verifypass = $_POST['verifyp'];
-
-	if ($join_password == $join_verifypass){
-		$emailcheck = "SELECT `Email` FROM `Users` WHERE `Email`='$join_email';";
-		$result = perform_query($dbc, $emailcheck);
-		if ($result->num_rows == 0){
-			$join_securepw = sha1($join_password);
-			$query = "INSERT INTO `Users` VALUES ( '$join_name', '$join_securepw', '$join_email')";
-			$adding = perform_query($dbc, $query);
-			disconnect_from_db($dbc, $adding);
-		}
-		else{
-			// want to make the errors appear in alert boxes
-			echo "<script language='javascript'>";
-			echo "alert('Error. Already existing email.')";
-			echo "</script>";
-			disconnect_from_db($dbc, $result);
-		}
-	}
-	else{
-		echo "<script language='javascript'>";
-		echo "alert('Error. Passwords do not match.')";
-		echo "</script>";
-	}
-
 ?>
