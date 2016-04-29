32836c04e3b03c8f53aa1da31e19938e1d63730b
an931a <hanav@bc.edu>
2016-04-29 19:21:26 -0400

    validate login?

---
+++ js/validatelogin.js
@@ -0,0 +1,52 @@
+/*
+function validatelogIn() {
+		var emailValid = validateloginEmail();
+		var pwValid = validateloginPw();
+		if (emailValid && pwValid)
+			//header("Location: http://")
+			return true;
+		return false;
+	}
+	
+function validateloginEmail() {
+	var email = document.forms["login"]["loginEmail"].value;
+	var emailRegex = /^\S+@\S+\.\S+$/;
+		if (email == null || email == "") {
+			alert("Please fill out your email.");
+			return false;
+		if ( !emailRegex.test(email.value) ) ;
+			alert("This email does not have an account!");
+			return false;
+		}
+		return true;
+		
+	}
+	
+function validateloginPw () {
+	var pw = document.forms["login"]["loginPassword"].value;
+		if (pw == null || pw == "") {
+			alert ("Please fill out your password.");
+			return false;
+		if (
+			alert("The password does not match the email provided.")
+			return false;
+		}
+
+		return true;
+	}
+	
+*/	
+	
+function validatelogIn() {
+        var email = document.loginform.loginEmail.value;
+        var pw = document.loginform.loginPw.value;
+        var loginemail = ""; 
+        var password = "";
+        if ((email == loginemail) && (pw == password)) {
+            return true;
+        }
+        else {
+            alert ("Login was unsuccessful, please check your email and password");
+            return false;
+        }
+  }
