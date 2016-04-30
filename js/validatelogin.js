db7d0ee9f02609e09916c7278be36071a7a32d50
an931a <hanav@bc.edu>
2016-04-29 20:43:38 -0400

    validate

--- js/validatelogin.js
+++ js/validatelogin.js
@@ -1,9 +1,9 @@
-/*
+
 function validatelogIn() {
 		var emailValid = validateloginEmail();
 		var pwValid = validateloginPw();
 		if (emailValid && pwValid)
-			//header("Location: http://")
+			//header("Location: .../memberpage.php")
 			return true;
 		return false;
 	}
@@ -12,11 +12,14 @@ function validateloginEmail() {
 	var email = document.forms["login"]["loginEmail"].value;
 	var emailRegex = /^\S+@\S+\.\S+$/;
 		if (email == null || email == "") {
-			alert("Please fill out your email.");
+			var errorrpt = document.getElementById("loginemailerror");
+			errorrpt.innerHTML = "Please fill out your email.";
 			return false;
-		if ( !emailRegex.test(email.value) ) ;
-			alert("This email does not have an account!");
+			}
+		if ( !emailRegex.test(email.value) ) {
+			errorpt.innerHTML = "This email does not have an account!";
 			return false;
+			}
 		}
 		return true;
 		
@@ -25,28 +28,29 @@ function validateloginEmail() {
 function validateloginPw () {
 	var pw = document.forms["login"]["loginPassword"].value;
 		if (pw == null || pw == "") {
-			alert ("Please fill out your password.");
+			var errorrpt = document.getElementById("loginpwerror");
+				errorrpt.innerHTML = "Please fill out your password.";
 			return false;
-		if (
+		if ()
 			alert("The password does not match the email provided.")
 			return false;
 		}
 
 		return true;
 	}
-	
-*/	
-	
+/*
 function validatelogIn() {
-        var email = document.loginform.loginEmail.value;
-        var pw = document.loginform.loginPw.value;
+        var email = document.forms["login"]["loginEmail"].value;
+        var pw = document.forms["login"]["loginPassword"].value;
         var loginemail = ""; 
         var password = "";
         if ((email == loginemail) && (pw == password)) {
             return true;
         }
         else {
-            alert ("Login was unsuccessful, please check your email and password");
+            var errorrpt = document.getElementById("loginpwerror");
+				errorrpt.innerHTML ="Login was unsuccessful, please check your email and password";
             return false;
         }
   }
+*/
