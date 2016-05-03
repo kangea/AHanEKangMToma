ca6c18980f6b1752ad5086d9122e059c343fb7b3
tomama1 <tomama@bc.edu>
2016-05-03 00:45:42 -0400

    draft 1 of youtube api

--- member/celebprofile.php
+++ member/celebprofile.php
@@ -31,6 +31,85 @@ if (!isset($_COOKIE['loginCookieUser'])){
 				 
 				  return t;
 				}(document, "script", "twitter-wjs"));</script>
+		<!-- Youtube -->
+		<script>
+		function handleAPILoaded() {
+  			$('#search-button').attr('disabled', false);
+			}
+
+		// Search for a specified string.
+		function search() {
+  			var q = $('#query').val();
+  			var request = gapi.client.youtube.search.list({
+    			q: q,
+ 		   part: 'snippet'
+ 			 });
+
+			  request.execute(function(response) {
+		    var str = JSON.stringify(response.result);
+    	$('#search-container').html('<pre>' + str + '</pre>');
+  			});
+		}
+
+		var OAUTH2_CLIENT_ID = '__YOUR_CLIENT_ID__';
+		var OAUTH2_SCOPES = [
+  	'https://www.googleapis.com/auth/youtube'
+];
+
+// Upon loading, the Google APIs JS client automatically invokes this callback.
+googleApiClientReady = function() {
+  gapi.auth.init(function() {
+    window.setTimeout(checkAuth, 1);
+  });
+}
+
+// Attempt the immediate OAuth 2.0 client flow as soon as the page loads.
+// If the currently logged-in Google Account has previously authorized
+// the client specified as the OAUTH2_CLIENT_ID, then the authorization
+// succeeds with no user intervention. Otherwise, it fails and the
+// user interface that prompts for authorization needs to display.
+function checkAuth() {
+  gapi.auth.authorize({
+    client_id: OAUTH2_CLIENT_ID,
+    scope: OAUTH2_SCOPES,
+    immediate: true
+  }, handleAuthResult);
+}
+
+// Handle the result of a gapi.auth.authorize() call.
+function handleAuthResult(authResult) {
+  if (authResult && !authResult.error) {
+    // Authorization was successful. Hide authorization prompts and show
+    // content that should be visible after authorization succeeds.
+    $('.pre-auth').hide();
+    $('.post-auth').show();
+    loadAPIClientInterfaces();
+  } else {
+    // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
+    // client flow. The current function is called when that flow completes.
+    $('#login-link').click(function() {
+      gapi.auth.authorize({
+        client_id: OAUTH2_CLIENT_ID,
+        scope: OAUTH2_SCOPES,
+        immediate: false
+        }, handleAuthResult);
+    });
+  }
+}
+
+// Load the client interfaces for the YouTube Analytics and Data APIs, which
+// are required to use the Google APIs JS client. More info is available at
+// http://code.google.com/p/google-api-javascript-client/wiki/GettingStarted#Loading_the_Client
+function loadAPIClientInterfaces() {
+  gapi.client.load('youtube', 'v3', function() {
+    handleAPILoaded();
+  });
+}
+
+
+
+		</script>
+
 	</head>
 
 <!-- NAVIGATION BAR -->
@@ -110,6 +189,20 @@ function displayTwitter(){
 	echo "<div class=\"container col-md-4\">
 		<a class=\"twitter-timeline\" href=\"$twitter\" data-widget-id=\"$twitid\">Tweets by @$celebName</a>
 		</div>";
+}
+
+function displayYoutube(){
+	//get CelebID
+	$dbc = connect_to_db("hanav");
+	$celebid = $_COOKIE['CelebID'];
+	$query = "SELECT * FROM Celebrities WHERE ID='$celebid';";
+	$result = perform_query($dbc, $query);
+	$obj = mysqli_fetch_object($result);
+	$youtube = ($obj->Youtube);
+	$youid = ($obj->YoutubeID);
+	$celebName = ($obj->CelebName);
+	disconnect_from_db($dbc, $result);
+	echo 
 }
 ?>
 </html>
