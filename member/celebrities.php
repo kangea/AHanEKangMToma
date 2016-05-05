9c9b608b0430a4740c4f2e9eba23fac6e681195a
an931a <hanav@bc.edu>
2016-05-05 00:53:40 -0400

    prettyy

--- member/celebrities.php
+++ member/celebrities.php
@@ -15,6 +15,7 @@ if (isset($_GET['celebbutton'])){
 		<title>CelebWatch | Celebrities</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="https://bootswatch.com/lumen/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href= "../css/stylesheet.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	</head>
@@ -94,7 +95,7 @@ function displayCelebsTable($str){
 
 	$result = perform_query($dbc, $query);
 	$rowsFound = mysqli_num_rows($result);
-	echo "<div class=\"panel panel-primary\">
+	echo "<div class=\"panel panel-info\">
 				<div class=\"panel-heading\">
 					<h3 class=\"panel-title\">Celebrities</h3>
 				</div>
